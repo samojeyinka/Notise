204cd753803cc96a9364a44a7933563ccc4998e2
samojeyinka <samuelojeyinka@gmail.com>
2023-12-21 19:31:46 -1200

    Add validations to the posts options

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+    before_validation :strip_whitespaces
+
+    validates :title, presence: true, length:{minimum:3, maximum:120}
+    validates :content, presence: true
+
+    def strip_whitespaces
+        self.title = title.strip if title.present?
+        self.content = content.strip if content.present?
+    end
 end

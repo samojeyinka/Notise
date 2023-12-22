7c60324c8ee2d97fb1fc82245b09d28c4e1ced07
samojeyinka <samuelojeyinka@gmail.com>
2023-12-21 18:32:50 -1200

    Added the posts actions in the controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,46 @@
 class Api::V1::PostsController < ApplicationController
+    before_action :set_post, only: [:show, :update, :destroy]
+
+      def index
+        @posts = Post.all
+        render json: @posts
+      end
+
+      def show
+        render json: @post
+      end
+
+      def create
+        @post = Post.new(post_params)
+        if @post.save
+          render json: @post, status: :created
+        else
+          render json: @post.errors, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @post.update(post_params)
+          render json: @post
+        else
+          render json: @post.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @post.destroy
+        head :no_content
+      end
+
+      private
+
+      def set_post
+        @post = Post.find(params[:id])
+      end
+
+      def post_params
+        params.require(:post).permit(:title, :content)
+      end
 end
+
+
